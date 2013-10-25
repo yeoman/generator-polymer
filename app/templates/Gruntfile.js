4af233f2e059dfbfd695e4c3d31997948222571b
Sindre Sorhus <sindresorhus@gmail.com>
2013-10-26 00:54:55 +0200

    Add jshint-stylish for some stylish jshinting
    https://github.com/sindresorhus/jshint-stylish

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -102,7 +102,8 @@ module.exports = function (grunt) {
         },
         jshint: {
             options: {
-                jshintrc: '.jshintrc'
+                jshintrc: '.jshintrc',
+                reporter: require('jshint-stylish')
             },
             all: [
                 'Gruntfile.js',
