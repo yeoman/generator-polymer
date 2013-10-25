9a38ea125d1c613d86c3ba5bd16944a9f9075cdd
Sindre Sorhus <sindresorhus@gmail.com>
2013-10-26 00:54:12 +0200

    Add load-grunt-tasks
    https://github.com/sindresorhus/load-grunt-tasks

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -12,9 +12,10 @@ var mountFolder = function (connect, dir) {
 // 'test/spec/**/*.js'
 
 module.exports = function (grunt) {
-    // load all grunt tasks
-    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+    // show elapsed time at the end
     require('time-grunt')(grunt);
+    // load all grunt tasks
+    require('load-grunt-tasks')(grunt);
 
     // configurable paths
     var yeomanConfig = {
