49fd0bbb09041197a79cf118c2276f1a837354e3
Rob Dodson <robdodson@google.com>
2014-04-24 08:02:55 -0700

    Restart generator with untitled-element

--- app/index.js
+++ app/index.js
@@ -1,59 +1,29 @@
 var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
-
+var _s = require('underscore.string');
 
 module.exports = Generator;
 
 function Generator(args, options, config) {
   yeoman.generators.Base.apply(this, arguments);
-
-  this.testFramework = this.options['test-framework'] || 'mocha';
-
-
-  this.hookFor(this.testFramework, {
-    as: 'app',
-    options: {
-      options: {
-        'skip-install': this.options['skip-install']
-      }
-    }
-  });
-
-  this.indexFile = this.readFileAsString(path.join(this.sourceRoot(), 'index.html'));
-
-/*
-  this.on('end', function () {
-    if (['app', 'polymer'].indexOf(this.generatorName) >= 0) {
-      this.installDependencies({ skipInstall: this.options['skip-install'] });
-    }
-  });
-*/
   this.options = options;
 }
 
 util.inherits(Generator, yeoman.generators.Base);
 
-Generator.prototype.askFor = function askFor() {
-  var cb = this.async();
-  //console.log(this.yeoman);
-  console.log('Out of the box I include HTML5 Boilerplate and Polymer.');
+// Generator.prototype.askFor = function askFor() {
+//   var cb = this.async();
+//   //console.log(this.yeoman);
+//   console.log('Out of the box I include the Polymer untitled-element.');
 
-  var prompts = [{
-    type: 'confirm',
-    name: 'compassBootstrap',
-    message: 'Include Bootstrap for Sass?',
-    default: true
-  }];
+//   this.prompt(prompts, function (props) {
+//     // manually deal with the response, get back and store the results.
+//     // we change a bit this way of doing to automatically do this in the self.prompt() method.
 
-  this.prompt(prompts, function (props) {
-    // manually deal with the response, get back and store the results.
-    // we change a bit this way of doing to automatically do this in the self.prompt() method.
-    this.compassBootstrap = props.compassBootstrap;
-
-    cb();
-  }.bind(this));
-};
+//     cb();
+//   }.bind(this));
+// };
 
 Generator.prototype.git = function git() {
   this.copy('gitignore', '.gitignore');
@@ -73,47 +43,25 @@ Generator.prototype.editorConfig = function editorConfig() {
   this.copy('editorconfig', '.editorconfig');
 };
 
-Generator.prototype.gruntfile = function gruntfile() {
-  this.template('Gruntfile.js');
-};
-
-Generator.prototype.packageJSON = function packageJSON() {
-  this.template('_package.json', 'package.json');
-};
-
-Generator.prototype.bootstrapImg = function bootstrapImg(){
-  if (this.compassBootstrap) {
-    this.copy('glyphicons-halflings.png', 'app/images/glyphicons-halflings.png');
-    this.copy('glyphicons-halflings-white.png', 'app/images/glyphicons-halflings-white.png');
-  }
-}
-
 Generator.prototype.mainStylesheet = function mainStylesheet() {
-  var css = 'main.' + (this.compassBootstrap ? 's' : '') + 'css';
-  this.copy(css, 'app/styles/' + css);
+  this.template('_untitled-element.css', _s.slugify(this.appname) + '.css');
+};
+
+Generator.prototype.mainImport = function mainImport() {
+  this.template('_untitled-element.html', _s.slugify(this.appname) + '.html');
 };
 
 Generator.prototype.writeIndex = function writeIndex() {
-  this.indexFile = this.readFileAsString(path.join(this.sourceRoot(), 'index.html'));
-  this.indexFile = this.engine(this.indexFile, this);
+  this.template('_index.html', 'index.html');
 };
 
-Generator.prototype.setupEnv = function setupEnv() {
-  this.mkdir('app');
-  this.mkdir('app/scripts');
-  this.mkdir('app/scripts/vendor/');
-  this.mkdir('app/styles');
-  this.mkdir('app/images');
-  this.mkdir('app/elements');
-  this.template('app/404.html');
-  this.template('app/favicon.ico');
-  this.template('app/robots.txt');
-  this.copy('app/htaccess', 'app/.htaccess');
-  this.copy('app/list.html', 'app/elements/list.html');
-  this.copy('app/greeting.html', 'app/elements/greeting.html');
-  this.write('app/index.html', this.indexFile);
+Generator.prototype.demo = function demo() {
+  this.template('_demo.html', 'demo.html');
 };
 
+Generator.prototype.readme = function readme() {
+  this.template('_README.md', 'README.md');
+};
 
 Generator.prototype.install = function () {
   if (this.options['skip-install']) {
@@ -124,6 +72,7 @@ Generator.prototype.install = function () {
   this.installDependencies({
     skipMessage: this.options['skip-install-message'],
     skipInstall: this.options['skip-install'],
+    npm: false,
     callback: done
   });
 }
---
+++ app/templates/_README.md
@@ -0,0 +1,8 @@
+<%= _.slugify(appname) %>
+================
+
+See the [component page](http://polymerlabs.github.io/<%= _.slugify(appname) %>) for more information.
+
+## Getting Started
+
+We've put together a [guide to <%= _.slugify(appname) %>](http://www.polymer-project.org/docs/start/reusableelements.html) to help get you rolling.
