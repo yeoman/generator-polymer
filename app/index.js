a9e6cca5cdb9de27994c5806df84aae99c528bad
Rob Dodson <robdodson@google.com>
2014-04-24 18:57:27 -0700

    Prompt for element name and generate a new directory

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
-var _s = require('underscore.string');
+var path = require('path');
 
 module.exports = Generator;
 
@@ -12,55 +12,62 @@ function Generator(args, options, config) {
 
 util.inherits(Generator, yeoman.generators.Base);
 
-// Generator.prototype.askFor = function askFor() {
-//   var cb = this.async();
-//   //console.log(this.yeoman);
-//   console.log('Out of the box I include the Polymer untitled-element.');
+Generator.prototype.askFor = function askFor() {
+  var cb = this.async();
+  //console.log(this.yeoman);
+  console.log('Out of the box I include the Polymer untitled-element.');
 
-//   this.prompt(prompts, function (props) {
-//     // manually deal with the response, get back and store the results.
-//     // we change a bit this way of doing to automatically do this in the self.prompt() method.
+  var prompts = [{
+    name: 'elementName',
+    message: 'What is your element name',
+    default: 'untitled-element'
+  }];
 
-//     cb();
-//   }.bind(this));
-// };
+  this.prompt(prompts, function (props) {
+    // manually deal with the response, get back and store the results.
+    // we change a bit this way of doing to automatically do this in the self.prompt() method.
+    this.elementName = props.elementName
+
+    cb();
+  }.bind(this));
+};
 
 Generator.prototype.git = function git() {
-  this.copy('gitignore', '.gitignore');
-  this.copy('gitattributes', '.gitattributes');
+  this.copy('gitignore', this.elementName + '/.gitignore');
+  this.copy('gitattributes', this.elementName + '/.gitattributes');
 };
 
 Generator.prototype.bower = function bower() {
-  this.copy('bowerrc', '.bowerrc');
-  this.copy('_bower.json', 'bower.json');
+  this.copy('bowerrc', this.elementName + '/.bowerrc');
+  this.copy('_bower.json', this.elementName + '/bower.json');
 };
 
 Generator.prototype.jshint = function jshint() {
-  this.copy('jshintrc', '.jshintrc');
+  this.copy('jshintrc', this.elementName + '/.jshintrc');
 };
 
 Generator.prototype.editorConfig = function editorConfig() {
-  this.copy('editorconfig', '.editorconfig');
+  this.copy('editorconfig', this.elementName + '/.editorconfig');
 };
 
 Generator.prototype.mainStylesheet = function mainStylesheet() {
-  this.template('_untitled-element.css', _s.slugify(this.appname) + '.css');
+  this.template('_untitled-element.css', this.elementName + '/' + this.elementName + '.css');
 };
 
 Generator.prototype.mainImport = function mainImport() {
-  this.template('_untitled-element.html', _s.slugify(this.appname) + '.html');
+  this.template('_untitled-element.html', this.elementName + '/' + this.elementName + '.html');
 };
 
 Generator.prototype.writeIndex = function writeIndex() {
-  this.template('_index.html', 'index.html');
+  this.template('_index.html', this.elementName + '/index.html');
 };
 
 Generator.prototype.demo = function demo() {
-  this.template('_demo.html', 'demo.html');
+  this.template('_demo.html', this.elementName + '/demo.html');
 };
 
 Generator.prototype.readme = function readme() {
-  this.template('_README.md', 'README.md');
+  this.template('_README.md', this.elementName + '/README.md');
 };
 
 Generator.prototype.install = function () {
@@ -68,11 +75,20 @@ Generator.prototype.install = function () {
     return;
   }
 
+  var origin = process.cwd();
+  process.chdir(path.join(origin, this.elementName));
+
   var done = this.async();
+  var installComplete = function() {
+    process.chdir(origin);
+    done();
+  };
+
   this.installDependencies({
     skipMessage: this.options['skip-install-message'],
     skipInstall: this.options['skip-install'],
     npm: false,
-    callback: done
+    callback: installComplete
   });
+
 }
--- app/templates/_README.md
+++ app/templates/_README.md
@@ -1,8 +1,8 @@
-<%= _.slugify(appname) %>
+<%= _.slugify(elementName) %>
 ================
 
-See the [component page](http://polymerlabs.github.io/<%= _.slugify(appname) %>) for more information.
+See the [component page](http://polymerlabs.github.io/<%= _.slugify(elementName) %>) for more information.
 
 ## Getting Started
 
-We've put together a [guide to <%= _.slugify(appname) %>](http://www.polymer-project.org/docs/start/reusableelements.html) to help get you rolling.
+We've put together a [guide to <%= _.slugify(elementName) %>](http://www.polymer-project.org/docs/start/reusableelements.html) to help get you rolling.
