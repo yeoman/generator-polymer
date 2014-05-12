866b69b479816a7dea934a87d3ca9d25fd6c0cd4
Rob Dodson <robdodson@google.com>
2014-05-12 07:28:31 -0700

    add wip gh-pages sub generator

--- app/index.js
+++ app/index.js
@@ -1,94 +1,56 @@
-var util = require('util');
-var path = require('path');
+'use strict';
 var yeoman = require('yeoman-generator');
 var path = require('path');
+var yosay = require('yosay');
 
-module.exports = Generator;
+module.exports = yeoman.generators.Base.extend({
+  init: function () {
+    this.on('end', function () {
+      if (!this.options['skip-install']) {
+        this.installDependencies({
+          skipMessage: this.options['skip-install-message'],
+          npm: false
+        });
+      }
+    });
+  },
+  askFor: function () {
+    var done = this.async();
 
-function Generator(args, options, config) {
-  yeoman.generators.Base.apply(this, arguments);
-  this.options = options;
-}
+    // Have Yeoman greet the user.
+    this.log(yosay('Out of the box I include the Polymer seed-element.'));
 
-util.inherits(Generator, yeoman.generators.Base);
+    var defaultName = path.basename(process.cwd());
+    var prompts = [
+      {
+        name: 'ghUser',
+        message: 'What is your GitHub username?'
+      },
+      {
+        name: 'elementName',
+        message: 'What is your element\'s name',
+        default: defaultName
+      }
+    ];
 
-Generator.prototype.askFor = function askFor() {
-  var cb = this.async();
-  //console.log(this.yeoman);
-  console.log('Out of the box I include the Polymer untitled-element.');
+    this.prompt(prompts, function (props) {
+      this.ghUser = props.ghUser;
+      this.elementName = props.elementName
 
-  var prompts = [{
-    name: 'elementName',
-    message: 'What is your element name',
-    default: 'untitled-element'
-  }];
-
-  this.prompt(prompts, function (props) {
-    // manually deal with the response, get back and store the results.
-    // we change a bit this way of doing to automatically do this in the self.prompt() method.
-    this.elementName = props.elementName
-
-    cb();
-  }.bind(this));
-};
-
-Generator.prototype.git = function git() {
-  this.copy('gitignore', this.elementName + '/.gitignore');
-  this.copy('gitattributes', this.elementName + '/.gitattributes');
-};
-
-Generator.prototype.bower = function bower() {
-  this.copy('bowerrc', this.elementName + '/.bowerrc');
-  this.copy('_bower.json', this.elementName + '/bower.json');
-};
-
-Generator.prototype.jshint = function jshint() {
-  this.copy('jshintrc', this.elementName + '/.jshintrc');
-};
-
-Generator.prototype.editorConfig = function editorConfig() {
-  this.copy('editorconfig', this.elementName + '/.editorconfig');
-};
-
-Generator.prototype.mainStylesheet = function mainStylesheet() {
-  this.template('_untitled-element.css', this.elementName + '/' + this.elementName + '.css');
-};
-
-Generator.prototype.mainImport = function mainImport() {
-  this.template('_untitled-element.html', this.elementName + '/' + this.elementName + '.html');
-};
-
-Generator.prototype.writeIndex = function writeIndex() {
-  this.template('_index.html', this.elementName + '/index.html');
-};
-
-Generator.prototype.demo = function demo() {
-  this.template('_demo.html', this.elementName + '/demo.html');
-};
-
-Generator.prototype.readme = function readme() {
-  this.template('_README.md', this.elementName + '/README.md');
-};
-
-Generator.prototype.install = function () {
-  if (this.options['skip-install']) {
-    return;
+      done();
+    }.bind(this));
+  },
+  app: function () {
+    this.copy('gitignore', '.gitignore');
+    this.copy('gitattributes', '.gitattributes');
+    this.copy('bowerrc', '.bowerrc');
+    this.template('_bower.json', 'bower.json');
+    this.copy('jshintrc', '.jshintrc');
+    this.copy('editorconfig', '.editorconfig');
+    this.template('_seed-element.css', this.elementName + '.css');
+    this.template('_seed-element.html', this.elementName + '.html');
+    this.template('_index.html', 'index.html');
+    this.template('_demo.html', 'demo.html');
+    this.template('_README.md', 'README.md');
   }
-
-  var origin = process.cwd();
-  process.chdir(path.join(origin, this.elementName));
-
-  var done = this.async();
-  var installComplete = function() {
-    process.chdir(origin);
-    done();
-  };
-
-  this.installDependencies({
-    skipMessage: this.options['skip-install-message'],
-    skipInstall: this.options['skip-install'],
-    npm: false,
-    callback: installComplete
-  });
-
-}
+});
--- app/templates/_README.md
+++ app/templates/_README.md
@@ -1,8 +1,8 @@
 <%= _.slugify(elementName) %>
 ================
 
-See the [component page](http://polymerlabs.github.io/<%= _.slugify(elementName) %>) for more information.
+See the [component page](http://<%= ghUser %>.github.io/<%= _.slugify(elementName) %>) for more information.
 
 ## Getting Started
 
-We've put together a [guide to <%= _.slugify(elementName) %>](http://www.polymer-project.org/docs/start/reusableelements.html) to help get you rolling.
+We've put together a [guide for <%= _.slugify(elementName) %>](http://www.polymer-project.org/docs/start/reusableelements.html) to help get you rolling.
