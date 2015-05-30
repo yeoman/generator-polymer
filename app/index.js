7f069ba7ff21f0d3354cc5e52c88a5ac347adc81
Rob Dodson <robdodson@google.com>
2015-05-30 09:47:52 -0700

    Update to 1.0.0 polymer starter kit

--- app/index.js
+++ app/index.js
@@ -38,37 +38,12 @@ module.exports = yeoman.generators.Base.extend({
     }.bind(this));
   },
   app: function () {
-    this.copy('.gitignore', '.gitignore');
-    this.copy('.gitattributes', '.gitattributes');
     this.copy('.bowerrc', '.bowerrc');
-    this.copy('.jshintrc', '.jshintrc');
-    this.copy('.jscsrc', '.jscsrc');
     this.copy('.editorconfig', '.editorconfig');
-    this.copy('gulpfile.js', 'gulpfile.js');
-    this.mkdir('app');
-    this.mkdir('app/styles');
-    this.mkdir('app/images');
-    this.mkdir('app/scripts');
-    this.mkdir('app/elements');
-    // this.copy('app/404.html');
-    // this.template('app/favicon.ico');
-    // this.template('app/robots.txt');
-    this.copy('app/styles/main.css', 'app/styles/main.css');
-    this.copy('app/scripts/app.js', 'app/scripts/app.js');
-    this.copy('app/.htaccess', 'app/.htaccess');
-    this.copy('app/elements/elements.html', 'app/elements/elements.html');
-    this.copy('app/elements/yo-list/yo-list.html', 'app/elements/yo-list/yo-list.html');
-    this.copy('app/elements/yo-greeting/yo-greeting.html', 'app/elements/yo-greeting/yo-greeting.html');
-    this.copy('app/index.html', 'app/index.html');
-
-    // Remove WCT if the user opted out
-    this.copy('package.json', 'package.json', function(file) {
-      var manifest =  JSON.parse(file);
-      if (!this.includeWCT) {
-        delete manifest.devDependencies['web-component-tester'];
-      }
-      return JSON.stringify(manifest, null, 2);
-    }.bind(this));
+    this.copy('.gitattributes', '.gitattributes');
+    this.copy('.gitignore', '.gitignore');
+    this.copy('.jscsrc', '.jscsrc');
+    this.copy('.jshintrc', '.jshintrc');
 
     this.copy('bower.json', 'bower.json', function(file) {
       var manifest =  JSON.parse(file);
@@ -79,10 +54,27 @@ module.exports = yeoman.generators.Base.extend({
       return JSON.stringify(manifest, null, 2);
     }.bind(this));
 
+    this.copy('gulpfile.js', 'gulpfile.js');
+    this.copy('LICENSE.md', 'LICENSE.md');
+
+    // Remove WCT if the user opted out
+    this.copy('package.json', 'package.json', function(file) {
+      var manifest =  JSON.parse(file);
+      if (!this.includeWCT) {
+        delete manifest.devDependencies['web-component-tester'];
+      }
+      return JSON.stringify(manifest, null, 2);
+    }.bind(this));
+
+    this.copy('README.md', 'README.md');
+
     if (this.includeWCT) {
       this.copy('wct.conf.js', 'wct.conf.js');
       this.directory('test', 'test');
     }
+
+    this.mkdir('app');
+    this.directory('app', 'app');
   },
   install: function () {
     this.installDependencies({
