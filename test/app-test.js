561108f531592bdc701b41abce412861ce71b58d
Rob Dodson <robdodson@google.com>
2015-07-21 11:51:50 -0700

    Update tests to check for WCT

--- test/app-test.js
+++ test/app-test.js
@@ -4,7 +4,7 @@ var path    = require('path');
 var helpers = require('yeoman-generator').test;
 var assert  = require('yeoman-generator').assert;
 
-describe('yo polymer:app test', function () {
+describe('yo polymer:app with WCT test', function () {
 
   before(function (done) {
     helpers.run(path.join(__dirname, '../app'))
@@ -33,6 +33,53 @@ describe('yo polymer:app test', function () {
     ];
 
     assert.file(expected);
+  });
+
+  it('includes WCT', function() {
+    assert.fileContent('bower.json', /web-component-tester/gm);
+    assert.fileContent('bower.json', /test-fixture/gm);
+    assert.fileContent('package.json', /web-component-tester/gm);
+    assert.fileContent('gulpfile.js', /^require\('web-component-tester'\).+/gm);
+  });
+
+});
+
+describe('yo polymer:app without WCT test', function () {
+
+  before(function (done) {
+    helpers.run(path.join(__dirname, '../app'))
+      .inDir(path.join(__dirname, './tmp'))
+      .withArguments(['--skip-install'])
+      .withPrompt({
+        includeWCT: false
+      })
+      .on('end', done);
+  });
+
+  it('creates expected files', function () {
+    var expected = [
+      '.editorconfig',
+      '.gitattributes',
+      '.gitignore',
+      '.jscsrc',
+      '.jshintrc',
+      'bower.json',
+      'gulpfile.js',
+      'LICENSE.md',
+      'package.json',
+      'README.md'
+    ];
+
+    assert.file(expected);
+  });
+
+  it('does not include WCT', function() {
+    assert.noFileContent('bower.json', /web-component-tester/gm);
+    assert.noFileContent('bower.json', /test-fixture/gm);
+    assert.noFileContent('package.json', /web-component-tester/gm);
+    assert.fileContent(
+      'gulpfile.js', /^\/\/\srequire\('web-component-tester'\).+/gm
+    );
   });
 
 });
--- test/seed-test.js
+++ test/seed-test.js
@@ -4,7 +4,7 @@ var path    = require('path');
 var helpers = require('yeoman-generator').test;
 var assert  = require('yeoman-generator').assert;
 
-describe('yo polymer:seed test', function () {
+describe('yo polymer:seed with WCT test', function () {
 
   before(function (done) {
     helpers.run(path.join(__dirname, '../seed'))
@@ -36,6 +36,30 @@ describe('yo polymer:seed test', function () {
   it('creates the correct bower.json content', function () {
     assert.fileContent('bower.json', /"name": "seed-el"/);
     assert.fileContent('bower.json', /"main": "seed-el.html"/);
+  });
+
+  it('includes WCT', function() {
+    assert.fileContent('bower.json', /web-component-tester/gm);
+  });
+
+});
+
+describe('yo polymer:seed without WCT test', function () {
+
+  before(function (done) {
+    helpers.run(path.join(__dirname, '../seed'))
+      .inDir(path.join(__dirname, './tmp'))
+      .withArguments(['seed-el'])
+      .withPrompt({
+        ghUser: 'test-user',
+        elementName: 'seed-el',
+        includeWCT: false
+      })
+      .on('end', done);
+  });
+
+  it('does not include WCT', function() {
+    assert.noFileContent('bower.json', /web-component-tester/gm);
   });
 
 });
