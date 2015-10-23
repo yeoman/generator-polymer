yeoman/generator-polymer

commit f9e2d0da558d0b71525e7019b14667d9f2381f42

Use generator v0.20. Close Gh-35 (Closes #229)

seed/index.js
@@ -73,38 +73,61 @@ module.exports = yeoman.generators.Base.extend({
   },
   seed: function () {
 
+    // Process function to replace 'seed-element' in template files
+    // with actual element name
     var renameElement = function (file) {
+      file = file.toString();
       return file.replace(/seed-element/g, this.elementName);
     }.bind(this);
 
-    // Handle bug where npm has renamed .gitignore to .npmignore
-    // https://github.com/npm/npm/issues/3763
-    if (this.src.isFile('.npmignore')) {
-      this.copy('.npmignore', '.gitignore');
-    } else {
-      this.copy('.gitignore', '.gitignore');
-    }
+    this.fs.copy([
+        this.templatePath() + '/**',
+        this.templatePath() + '/**/.*',
+        '!**/{bower.json,seed-element.html,.git,.npmignore,test}/**'],
+      this.destinationPath(),
+      { process: renameElement });
 
-    this.copy('bower.json', 'bower.json', function(file) {
-      var manifest = JSON.parse(file);
-      manifest.name = this.elementName;
-      manifest.main = this.elementName + '.html';
-      if (!this.includeWCT) {
-        delete manifest.devDependencies['web-component-tester'];
-        delete manifest.devDependencies['test-fixture'];
-      }
-      return JSON.stringify(manifest, null, 2);
-    }.bind(this));
+    this.fs.copy(
+      this.templatePath('seed-element.html'),
+      this.destinationPath(this.elementName + '.html'),
+      { process: renameElement });
 
-    this.copy('index.html', 'index.html', renameElement);
-    this.copy('README.md', 'README.md', renameElement);
-    this.copy('seed-element.html', this.elementName + '.html', renameElement);
-    this.copy('demo/index.html', 'demo/index.html', renameElement);
+    // Remove WCT if the user opted out
+    this.fs.copy(
+      this.templatePath('bower.json'),
+      this.destinationPath('bower.json'),
+      { process: function (file) {
+        var manifest =  JSON.parse(file.toString());
+        manifest.name = this.elementName;
+        manifest.main = this.elementName + '.html';
+        if (!this.includeWCT) {
+          delete manifest.devDependencies['web-component-tester'];
+          delete manifest.devDependencies['test-fixture'];
+        }
+        return JSON.stringify(manifest, null, 2);
+      }.bind(this) });
 
     if (this.includeWCT) {
-      this.copy('test/index.html', 'test/index.html', renameElement);
-      this.copy('test/basic-test.html', 'test/basic-test.html', renameElement);
+      this.fs.copy(
+        this.sourceRoot() + '/test/*',
+        this.destinationPath('test'),
+        { process: renameElement });
     }
+
+    // Handle bug where npm has renamed .gitignore to .npmignore
+    // https://github.com/npm/npm/issues/3763
+    if (this.fs.exists('.npmignore')) {
+      this.fs.copy(
+        this.templatePath('.npmignore'),
+        this.destinationPath('.gitignore')
+      );
+    } else {
+      this.fs.copy(
+        this.templatePath('.gitignore'),
+        this.destinationPath('.gitignore')
+      );
+    }
+
   },
   install: function () {
     this.installDependencies({

app/index.js
@@ -3,6 +3,7 @@ var yeoman = require('yeoman-generator');
 var path = require('path');
 var yosay = require('yosay');
 var chalk = require('chalk');
+var mkdirp = require('mkdirp');
 
 module.exports = yeoman.generators.Base.extend({
   constructor: function () {
@@ -47,62 +48,84 @@ module.exports = yeoman.generators.Base.extend({
     }.bind(this));
   },
   app: function () {
-    this.copy('.editorconfig', '.editorconfig');
-    this.copy('.gitattributes', '.gitattributes');
+
+    this.fs.copy([
+                                this.templatePath() + '/**',
+                                this.templatePath() + '/**/.*',
+                                '!**/{gulpfile.js,bower.json,package.json,.git,.npmignore, wct.conf.json,docs,test}/**'],
+                        this.destinationPath());
 
     // Handle bug where npm has renamed .gitignore to .npmignore
     // https://github.com/npm/npm/issues/3763
-    if (this.src.isFile('.npmignore')) {
-      this.copy('.npmignore', '.gitignore');
+    if (this.fs.exists('.npmignore')) {
+      this.fs.copy(
+        this.templatePath('.npmignore'),
+        this.destinationPath('.gitignore')
+      );
     } else {
-      this.copy('.gitignore', '.gitignore');
+      this.fs.copy(
+        this.templatePath('.gitignore'),
+        this.destinationPath('.gitignore')
+      );
     }
-    this.copy('.jscsrc', '.jscsrc');
-    this.copy('.jshintrc', '.jshintrc');
-
-    this.copy('bower.json', 'bower.json', function(file) {
-      var manifest =  JSON.parse(file);
-      if (!this.includeWCT) {
-        delete manifest.devDependencies['web-component-tester'];
-        delete manifest.devDependencies['test-fixture'];
-      }
-      return JSON.stringify(manifest, null, 2);
-    }.bind(this));
-
-    this.copy('gulpfile.js', 'gulpfile.js', function(file) {
-      var clone = file;
-      if (!this.includeWCT) {
-        clone = file.replace(/require\('web-component-tester'\).+/g,
-          function(match) {
-            return '// ' + match;
-          });
-      }
-      return clone;
-    }.bind(this));
-
-    this.copy('LICENSE.md', 'LICENSE.md');
 
     // Remove WCT if the user opted out
-    this.copy('package.json', 'package.json', function(file) {
-      var manifest =  JSON.parse(file);
-      if (!this.includeWCT) {
-        delete manifest.devDependencies['web-component-tester'];
-      }
-      return JSON.stringify(manifest, null, 2);
-    }.bind(this));
+    this.fs.copy(
+      this.templatePath('bower.json'),
+      this.destinationPath('bower.json'),
+      { process: function (file) {
+        var manifest =  JSON.parse(file.toString());
+        if (!this.includeWCT) {
+          delete manifest.devDependencies['web-component-tester'];
+          delete manifest.devDependencies['test-fixture'];
+        }
+        return JSON.stringify(manifest, null, 2);
+      }.bind(this) });
 
-    this.copy('README.md', 'README.md');
+    // Remove WCT if the user opted out
+    this.fs.copy(
+      this.templatePath('gulpfile.js'),
+      this.destinationPath('gulpfile.js'),
+      { process: function (file) {
+        var clone = file.toString();
+        if (!this.includeWCT) {
+          clone = clone.replace(/require\('web-component-tester'\).+/g,
+            function(match) {
+              return '// ' + match;
+            });
+        }
+        return clone;
+      }.bind(this) });
+
+    // Remove WCT if the user opted out
+    this.fs.copy(
+      this.templatePath('package.json'),
+      this.destinationPath('package.json'),
+      { process: function (file) {
+        var manifest =  JSON.parse(file.toString());
+        if (!this.includeWCT) {
+          delete manifest.devDependencies['web-component-tester'];
+        }
+        return JSON.stringify(manifest, null, 2);
+      }.bind(this) });
 
     if (this.includeWCT) {
-      this.copy('wct.conf.json', 'wct.conf.json');
-      this.directory('test', 'test');
+      this.fs.copy(
+        this.templatePath('wct.conf.json'),
+        this.destinationPath('wct.conf.json')
+      );
+
+      this.fs.copy(
+        this.templatePath('app/test'),
+        this.destinationPath('app/test')
+      );
     }
 
-    this.mkdir('app');
-    this.directory('app', 'app');
-
     if (this.includeRecipes) {
-      this.directory('docs', 'docs');
+      this.fs.copy(
+        this.templatePath('docs'),
+        this.destinationPath('docs')
+      );
     }
   },
   install: function () {

el/index.js
@@ -1,6 +1,9 @@
 'use strict';
 var yeoman = require('yeoman-generator');
 var path = require('path');
+var htmlWiring = require('html-wiring');
+var readFileAsString = htmlWiring.readFileAsString;
+var writeFileFromString = htmlWiring.writeFileFromString;
 
 module.exports = yeoman.generators.Base.extend({
   constructor: function () {
@@ -74,70 +77,85 @@ module.exports = yeoman.generators.Base.extend({
 
     if (this.flags.path) {
 
-      // --path foo/bar
-      // el = "foo/bar/x-foo"
-      el = path.join(this.flags.path, this.elementName);
-
-      // pathToEl = "app/elements/foo/bar/x-foo"
-      pathToEl = path.join('app/elements', el);
+      // pathToEl = "app/elements/foo/bar/"
+      pathToEl = path.join(this.destinationPath('app/elements'), this.flags.path);
 
     } else {
 
-      // el = "x-foo/x-foo"
-      el = path.join(this.elementName, this.elementName);
-
-      // pathToEl = "app/elements/x-foo/x-foo"
-      pathToEl = path.join('app/elements', el);
+      // pathToEl = "app/elements/x-foo/"
+      pathToEl = path.join(this.destinationPath('app/elements'), this.elementName);
 
     }
 
     // Used by element template
-    this.pathToBower = path.relative(
-      path.dirname(pathToEl),
-      path.join(process.cwd(), 'app/bower_components')
+    var tpl = {
+      elementName: this.elementName,
+      components: this.components,
+      pathToBower: path.relative(
+          pathToEl,
+          path.join(process.cwd(), 'app/bower_components')
+        )
+    };
+
+    this.fs.copyTpl(
+      path.join(this.templatePath('element.html')),
+      path.join(pathToEl, this.elementName + '.html'),
+      tpl
     );
-    this.template(path.join(__dirname, 'templates/element.html'), pathToEl + '.html');
 
     // Wire up the dependency in elements.html
     if (this.includeImport) {
-      var file = this.readFileAsString('app/elements/elements.html');
+      var file = readFileAsString(this.destinationPath('app/elements/elements.html'));
       el = el.replace(/\\/g, '/');
       file += '<link rel="import" href="' + el + '.html">\n';
-      this.writeFileFromString(file, 'app/elements/elements.html');
+      writeFileFromString(file, this.destinationPath('app/elements/elements.html'));
     }
 
     if (this.testType && this.testType !== 'None') {
-      var testDir = 'app/test';
+      var testDir = this.destinationPath('app/test');
 
       if (this.testType === 'TDD') {
-        this.template(path.join(__dirname, 'templates/test/_tdd.html'), path.join(testDir, this.elementName+'-basic.html'));
+        this.fs.copyTpl(
+          this.templatePath('test/tdd.html'),
+          path.join(testDir, this.elementName+'-basic.html'),
+          tpl
+        );
       } else if (this.testType === 'BDD') {
-        this.template(path.join(__dirname, 'templates/test/_bdd.html'), path.join(testDir, this.elementName+'-basic.html'));
+        this.fs.copyTpl(
+          this.templatePath('test/bdd.html'),
+          path.join(testDir, this.elementName+'-basic.html'),
+          tpl
+        );
       }
 
       // Open index.html, locate where to insert text, insert ", x-foo.html" into the array of components to test
       var indexFileName = 'app/test/index.html';
-      var origionalFile = this.readFileAsString(indexFileName);
+      var origionalFile = readFileAsString(this.destinationPath(indexFileName));
       var regex = /WCT\.loadSuites\(\[([^\]]*)/;
       var match = regex.exec(origionalFile);
       var indexOfInsertion = match.index + match[0].length;
       var comma = (match[1].length === 0) ? '' : ', ';
       var newFile = origionalFile.slice(0, indexOfInsertion) + comma + '\'' + this.elementName + '-basic.html\'' + origionalFile.slice(indexOfInsertion);
-      this.writeFileFromString(newFile, indexFileName);
+      writeFileFromString(newFile, indexFileName);
     }
 
     // copy documentation page and demo page only if flag is set
     if (this.flags.docs) {
-      var elementDir = 'app/elements';
 
-      // pathToElementDir = "app/elements/x-foo"
-      var pathToElementDir = path.join(elementDir, this.elementName);
+      // copy templates/index.html -> app/elements/x-foo/index.html (documentation page)
+      this.fs.copyTpl(
+        this.templatePath('index.html'),
+        path.join(pathToEl, 'index.html'),
+        tpl
+      );
 
-      // copy templates/_index.html -> app/elements/x-foo/index.html (documentation page)
-      this.template(path.join(__dirname, 'templates/_index.html'), path.join(pathToElementDir, 'index.html'));
+      // copy templates/demo.html -> app/elements/x-foo/demo.html (demo page)
+      this.fs.copyTpl(
+        this.templatePath('demo.html'),
+        path.join(pathToEl, 'demo/index.html'),
+        tpl
+      );
 
-      // copy templates/_demo.html -> app/elements/x-foo/demo.html (demo page)
-      this.template(path.join(__dirname, 'templates/_demo.html'), path.join(pathToElementDir, 'demo/index.html'));
     }
   }
 });

gh/index.js
@@ -2,7 +2,6 @@
 var yeoman = require('yeoman-generator');
 var path = require('path');
 var yosay = require('yosay');
-var ncp = require('ncp');
 var spawn = require('child_process').spawn;
 var rimraf = require('rimraf');
 
@@ -40,33 +39,37 @@ module.exports = yeoman.generators.Base.extend({
 
       done();
     }.bind(this));
+
+    // Create a tmp dir to run our process in
+    this.destinationRoot('tmp-' + Date.now());
   },
-  gh: function () {
+  copy: function () {
+    this.fs.copy(
+      this.templatePath('gp.sh'),
+      this.destinationPath('gp.sh')
+    );
+  },
+  end: function () {
     var done = this.async();
-    var src = path.join(__dirname, 'templates');
-    var dest = path.join(process.cwd(), 'tmp-' + Date.now());
 
-    // work around weird timing issues with this.copy...
-    ncp(src, dest, function (err) {
-      if (err) {
-        return this.log(err);
-      }
+    var gp = spawn(
+      'bash',
+      ['gp.sh', this.ghUser, this.elementName, 'master', this.includeDevDeps],
+      {cwd: this.destinationRoot()}
+    );
 
-      var gp = spawn('bash', ['gp.sh', this.ghUser, this.elementName, 'master', this.includeDevDeps], {cwd: dest});
+    gp.stdout.on('data', function (data) {
+      this.log(data.toString());
+    }.bind(this));
 
-      gp.stdout.on('data', function (data) {
-        this.log(data.toString());
-      }.bind(this));
+    gp.stderr.on('data', function (data) {
+      this.log(data.toString());
+    }.bind(this));
 
-      gp.stderr.on('data', function (data) {
-        this.log(data.toString());
-      }.bind(this));
-
-      gp.on('close', function (code) {
-        this.log('child process exited with code ' + code);
-        rimraf.sync(dest);
-        done();
-      }.bind(this));
+    gp.on('close', function (code) {
+      this.log('child process exited with code ' + code);
+      rimraf.sync(this.destinationRoot());
+      done();
     }.bind(this));
   }
 });

commit 4d059e465a4afbbe630744819b23e1bfc1168826

Fix incorrect npmignore sniffing
Fixes #240

Regression introduced in
https://github.com/yeoman/generator-polymer/commit/f9e2d0da558d0b71525e7019b14667d9f2381f42#diff-2018087f584c4398b5c3a23fc0e5f9dbR60

seed/index.js
@@ -116,7 +116,7 @@ module.exports = yeoman.generators.Base.extend({
 
     // Handle bug where npm has renamed .gitignore to .npmignore
     // https://github.com/npm/npm/issues/3763
-    if (this.fs.exists('.npmignore')) {
+    if (this.fs.exists(this.templatePath('.npmignore'))) {
       this.fs.copy(
         this.templatePath('.npmignore'),
         this.destinationPath('.gitignore')